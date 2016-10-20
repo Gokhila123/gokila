8c69a4c8dbdcaa0849f1bd82bdf978b69cb7ef55
Gokhila123 <gokilabalan1@gmail.com>
2016-10-20 12:01:10 +0530

    Update and rename Sum.java to Summer.java

---
+++ Summer.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Summer{
+public static void main(String args[])
+{
+int a=100,sum=0,i;
+for(i=0;i<=a;i++)
+{
+sum=sum+i;
+}
+System.out.pritln("sum of"+sum);
+}
+}
