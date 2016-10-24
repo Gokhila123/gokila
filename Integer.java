7147ab53c3fd98a1ff9ad93704961c04ed086eba
Gokhila123 <gokilabalan1@gmail.com>
2016-10-24 09:33:37 +0530

    Create Integer.java

---
+++ Integer.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Integer{
+public  static void main(String args[]){
+String number = "10";
+Integer result = Integer.valueOf(number);
+System.out.println(result);
+}
+}
